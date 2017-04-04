0d825747c637edd085666e50898f55b6d968f943
bill <bill@williamwheatley.com>
2017-04-04 18:12:50 -0400

    Adding the ability to build a mesh out of a sprite, and then save the mesh for future use.
    An example without the mesh set and with it set. Lets you use water effect on any possible shape based off a sprite vs having to use a square.

--- Assets/Water2D Surface/scripts/Water2DScript.cs
+++ Assets/Water2D Surface/scripts/Water2DScript.cs
@@ -1,27 +1,62 @@
 ﻿// 2016 - Damien Mayance (@Valryon)
 // Source: https://github.com/valryon/water2d-unity/
+
 using UnityEngine;
+using System;
+
 
 /// <summary>
-/// Water surface script (update the shader properties).
+///     Water surface script (update the shader properties).
 /// </summary>
-public class Water2DScript : MonoBehaviour
-{
-  public Vector2 speed = new Vector2(0.01f, 0f);
+public class Water2DScript : MonoBehaviour {
+    private Material mat;
 
-  private Renderer rend;
-  private Material mat;
+    [Tooltip("By default use a mesh, other times use a sprite?")]
+    public bool material = true;
 
-  void Awake()
-  {
-    rend = GetComponent<Renderer>();
-    mat = rend.material;
-  }
+    private Renderer rend;
+    private MeshFilter mf;
+    public Vector2 speed = new Vector2(0.01f, 0f);
 
-  void LateUpdate()
-  {
-    Vector2 scroll = Time.deltaTime * speed;
+    [Tooltip("Drag the sprite you'd like a mesh off for the water terrain")]
+    public Sprite theSprite;
+    public bool generateNewMesh = false;
 
-    mat.mainTextureOffset += scroll;
-  }
+    void Awake() {
+
+        //Convert the sprite to a mesh
+        mf = this.GetComponent<MeshFilter>();
+        mf.sharedMesh = SpriteToMesh(theSprite);
+
+//Only run this in the editor since this piece uses Editor only pieces        
+#if UNITY_EDITOR
+        if (generateNewMesh) {
+            var path = "Assets/Water2D Surface/demo/meshes/";
+            var filename = string.Format("WaterHexMesh.asset");
+            var fullpath = string.Format("{0}{1}", path, filename);
+            UnityEditor.AssetDatabase.CreateAsset(SpriteToMesh(theSprite), fullpath);
+        }
+#endif
+
+
+        rend = GetComponent<Renderer>();
+        mat = rend.material;
+    }
+
+    private void LateUpdate() {
+        var scroll = Time.deltaTime * speed;
+        mat.mainTextureOffset += scroll;
+
+    }
+
+    private static Mesh SpriteToMesh(Sprite sprite) {
+        Mesh mesh = new Mesh();
+        mesh.vertices = Array.ConvertAll(sprite.vertices, i => (Vector3)i);
+        mesh.uv = sprite.uv;
+        mesh.triangles = Array.ConvertAll(sprite.triangles, i => (int)i);
+
+        return mesh;
+    }
+
+
 }
